374858b90a037f84dfbbd1558dcac907ffcbc59f
dex <morgan_borley@yahoo.com.au>
2020-06-19 14:44:38 +1000

    adding login function

---
+++ app/src/main/java/com/jemena/maintenance/activity/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.jemena.maintenance.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jemena.maintenance.R;
+
+import java.lang.reflect.Array;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText _txtUser, _txtPass;
+    Button _btnLogin;
+    Spinner _spinner;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        _txtPass=(EditText)findViewById(R.id.txtPass);
+        _txtUser=(EditText)findViewById(R.id.txtUser);
+        _btnLogin=(Button)findViewById(R.id.btnLogin);
+        _spinner=(Spinner)findViewById(R.id.spinner);
+        ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.userType, R.layout.support_simple_spinner_dropdown_item);
+        _spinner.setAdapter(adapter);
+        _btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = _spinner.getSelectedItem().toString();
+                if(_txtUser.getText().toString().equals("admin")&& _txtPass.getText().toString().equals("admin")&& item.equals("admin")){
+                    Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                    startActivity(intent);
+
+                }else if(_txtUser.getText().toString().equals("admin")&& _txtPass.getText().toString().equals("admin")&& item.equals("user")){
+                    Intent intent = new Intent(LoginActivity.this, TechActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+}
